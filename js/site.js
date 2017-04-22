de57c1ab5af7620b5d8bf24667a448c5f5e9623f
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-04-22 22:18:56 +0530

    Nav

--- js/site.js
+++ js/site.js
@@ -2,10 +2,13 @@ $('.burger').click(function(){
     $('.ham').toggleClass('\glyphicon-menu-hamburger');
     $('.ham').toggleClass('glyphicon-chevron-left');
     if($('.links').css('display') == 'none'){
+        $("nav").css("background-color", "rgba(150,125,255,.9)");
+    //$('nav').css('bacground-color','rgba(255,255,255,.7)');
     $('.links').slideDown('500');
     }
     else{
        $('.links').slideUp('500'); 
+        $("nav").css("background-color", "transparent");
     }
 })
 
