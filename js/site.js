45971a2ea52f15942b05c59e1605c3e400bc5889
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-06-02 15:51:12 +0530

    nav bar links done

--- js/site.js
+++ js/site.js
@@ -1,6 +1,59 @@
-$( document ).ready(function(){
-    $(".button-collapse").sideNav();
-    $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+$('document').ready(function(){
+    console.log('Ready');
+    var pf = $('#portfolio').offset().top;
+        var about = $("#about").offset().top;
+        var contact = $('#contact').offset().top;
+    console.log(pf,about,contact);
+
+    $(window).scroll(function(){
+        var scrolled = $(window).scrollTop();
+        var height = $(window).height()
+       // var scbtm = scrolled + + $(window).height();
+        if(scrolled >= 50){
+            $('nav').addClass('shrink');
+        }
+        else{
+            $('nav').removeClass('shrink')
+        }
+
+
+
+        if(scrolled>=pf-100 && scrolled <about-100){
+            $('.pf-btn').addClass('active');
+            $('.ab-btn').removeClass('active');
+            $('.cn-btn').removeClass('active');
+        }
+
+        else if(scrolled >= about-100 && scrolled < contact-100){
+            $('.pf-btn').removeClass('active');
+            $('.ab-btn').addClass('active');
+            $('.cn-btn').removeClass('active');
+        }
+
+        else if(scrolled >= contact-100){
+            $('.pf-btn').removeClass('active');
+            $('.ab-btn').removeClass('active');
+            $('.cn-btn').addClass('active');
+        }
+
+        else{
+            $('.pf-btn').removeClass('active');
+            $('.ab-btn').removeClass('active');
+            $('.cn-btn').removeClass('active');
+        }
+        });
+
+    $('#img').click(function(){
+        console.log('Clicked');
+    })
+
+    $('#img').hover(function(){
+        $('.caption-content').show(500);
+        $(this).css({opacity:.5+'px'});
+        console.log('Show');
+    },function(){
+        $('.caption-content').hide(500);
+        $(this).css({opacity:1+'px'});
+        console.log('Hide');
+    })
 })
